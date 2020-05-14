098decc583393a7f7c6139de0abdcc879739f7fd
josephmotha1998 <josephmotha@outlook.com>
2020-05-14 12:37:22 -0400

    Added screen loader

--- Scripts/firstPage.js
+++ Scripts/firstPage.js
@@ -3,3 +3,24 @@ const scroll = new SmoothScroll('.navbar a[href*="#"]',{
     speed:800
 });
 
+$(document).ready(function(){
+    setTimeout(function(){
+        $('body').addClass('loaded');
+
+    },3000);
+});
+// $('.navbar a').on('click', function (e) {
+//   if (this.hash !== '') {
+//     e.preventDefault();
+
+//     const hash = this.hash;
+
+//     $('html, body')
+//       .animate({
+//         scrollTop: $(hash).offset().top
+//       },800);
+//   }
+// });
+
+
+
